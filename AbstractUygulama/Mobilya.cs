a60f0f46cdedb18d282307ef57889f364d6eb454
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-13 18:40:52 +0300

    Proje dosyası ekle.

---
+++ AbstractUygulama/Mobilya.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractUygulama
+{
+    abstract class Mobilya //abstractan obje tanımlanmaz fakat türetilmiş sınıflardan tanımlanabilir
+    {
+        public string renk;
+        abstract public void ozellikyaz();//bildirim
+    }
+    class kanepe : Mobilya
+    {
+        public string kumas;
+
+        public override void ozellikyaz()
+        {
+            Console.WriteLine("Kanepenin özellikleri:");
+            Console.WriteLine("Renk:{0}",renk);
+            Console.WriteLine("Kumaş:{0}",kumas);
+        }
+    }
+
+    class masa:Mobilya
+    {
+        public string malzeme;
+
+        public override void ozellikyaz()
+        {
+            Console.WriteLine("Masanın özellikleri");
+            Console.WriteLine("Renk:{0}", renk);
+            Console.WriteLine("Malzeme:{0}", malzeme);
+
+        }
+    }
+}
---
+++ AbstractUygulama/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractUygulama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            kanepe knp1 = new kanepe();
+            masa calisma_masasi = new masa();
+            knp1.renk = "pembe";
+            knp1.kumas = "Deri";
+            calisma_masasi.renk = "Siyah";
+            calisma_masasi.malzeme = "Ahşap";
+            knp1.ozellikyaz();
+            calisma_masasi.ozellikyaz();
+            Console.ReadKey();
+        }
+    }
+}
